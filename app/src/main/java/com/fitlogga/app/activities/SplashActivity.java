89a7e1ab5a1f53765774a1193228b65bddbbc66b
andrewdupree <andrewdupree@delta.edu>
2019-08-04 14:58:43 -0400

    Better ApplicationContext retrieval

--- app/src/main/java/com/fitlogga/app/activities/SplashActivity.java
+++ app/src/main/java/com/fitlogga/app/activities/SplashActivity.java
@@ -5,15 +5,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.fitlogga.app.models.ApplicationContext;
-
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        ApplicationContext.init(getApplicationContext());
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
--- app/src/main/java/com/fitlogga/app/models/ApplicationContext.java
+++ app/src/main/java/com/fitlogga/app/models/ApplicationContext.java
@@ -1,21 +1,19 @@
 package com.fitlogga.app.models;
 
 import android.annotation.SuppressLint;
+import android.app.Application;
 import android.content.Context;
 
-public class ApplicationContext {
+public class ApplicationContext extends Application {
 
     @SuppressLint("StaticFieldLeak")
     // ApplicationContext is a singleton by itself. There is no memory leak.
     private static Context context = null;
 
-    /**
-     * Loaded from SplashActivity.
-     */
-    public static void init(Context context) {
-        if (ApplicationContext.context == null) {
-            ApplicationContext.context = context;
-        }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ApplicationContext.context = getApplicationContext();
     }
 
     public static Context getInstance() {
